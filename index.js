6350eca782aac5c688a814d7b355884176cda420
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2023-03-02 11:20:59 +0000

    Added 2 patterns

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const readlineSync = require('readline-sync');
 const n = readlineSync.question('Enter value of n\n');
 
-function printPattern(n){
-  if(!(n >0 && n <=20)) return;
+function printPattern(n) {
   let str = '';
   for (let i = 1; i <= n; i++) {
-    for (let j = 0; j < i; j++) {
-      str += '* ';
+    for (let j = 1; j <= i; j++) {
+      str += `${i} `;
+    }
+    console.log(str);
+    str = '';
+  }
+}
+
+printPattern(n);
+
+
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+
+const readlineSync = require('readline-sync');
+const n = readlineSync.question('Enter value of n\n');
+
+function printPattern(n) {
+  let str = '';
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      str += `${j} `;
     }
     console.log(str);
     str = '';
